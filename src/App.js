d557c2a1aa58843a26816391463076f58d7006b7
eithan31sirusi <eithan26sirusi@gmail.com>
2021-04-06 18:19:21 +0300

    building the header compontent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/navbar/navbar.component";
+import Header from "./components/header/header.component";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              denstiy: {
+                ebable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              stroke: {
+                width: 6,
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./header.style.css";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-warper">
+      <div className="main-info">
+        <h1>work!</h1>
+        <Typed
+          className="typed-text"
+          strings={["Web Design", "Web-Developmant", "Responsive Banners "]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          contact us
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
